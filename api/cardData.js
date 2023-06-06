d1466216b18426105f4aceaabfe667a204e9afb2
Stacey Vanyo <sbvanyo@gmail.com>
2023-06-05 21:35:35 -0500

    addCardForm + dom/form/nav event listeners

--- api/cardData.js
+++ api/cardData.js
@@ -2,6 +2,7 @@ import client from '../utils/client';
 
 const endpoint = client.databaseURL;
 
+// GET CARDS
 const getCardData = (uid) => new Promise((resolve, reject) => {
   fetch(`${endpoint}/cards.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
@@ -21,4 +22,36 @@ const getCardData = (uid) => new Promise((resolve, reject) => {
   console.warn(uid);
 });
 
-export default getCardData;
+// CREATE CARD
+const createCard = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/cards.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// UPDATE BOOK
+const updateCard = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/cards/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getCardData,
+  createCard,
+  updateCard
+};
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,17 +1,21 @@
-import getCardData from '../api/cardData';
+import { getCardData } from '../api/cardData';
 import logoutButton from '../components/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
 import navBar from '../components/shared/navBar';
+import domEvents from '../events/domEvents';
+import formEvents from '../events/formEvents';
 import navigationEvents from '../events/navigationEvents';
 import { showCards } from '../pages/cards';
 
 const startApp = (user) => {
   domBuilder(user);
+  domEvents(user);
+  formEvents(user);
   navBar();
   logoutButton();
   navigationEvents(user);
 
-  // renders books to DOM on app load
+  // renders cardss to DOM on app load
   getCardData(user.uid).then((cards) => showCards(cards));
 };
 
