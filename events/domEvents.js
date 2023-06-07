ed2bb80973ce7f37628d94b82aa90ee3ea243dd1
Stacey Vanyo <sbvanyo@gmail.com>
2023-06-06 20:50:20 -0500

    update card functional

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,9 +1,10 @@
 // import addCardForm from '../components/forms/addCardForm';
-import { deleteCard, getCardData } from '../api/cardData';
+import { deleteCard, getCardData, getSingleCard } from '../api/cardData';
+import addCardForm from '../components/forms/addCardForm';
 import { showCards } from '../pages/cards';
 
 const domEvents = (user) => {
-  // DELETE A BOOK ON CLICK
+  // DELETE A CARD CLICK EVENT
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('delete-card')) {
       console.warn('clicked delete button', e.target.id);
@@ -12,6 +13,14 @@ const domEvents = (user) => {
       deleteCard(firebaseKey).then(() => {
         getCardData(user.uid).then(showCards);
       });
+    }
+
+    // UPDATE A CARD CLICK EVENT
+    if (e.target.id.includes('update-card')) {
+      console.warn('clicked edit button', e.target.id);
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleCard(firebaseKey).then((cardObj) => addCardForm(cardObj, user));
     }
   });
 };
--- events/formEvents.js
+++ events/formEvents.js
@@ -14,12 +14,30 @@ const formEvents = (user) => {
         uid: user.uid,
       };
 
+      document.querySelector('#submit-card').reset();
+
       createCard(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
 
         updateCard(patchPayload).then(() => {
           getCardData(user.uid).then(showCards);
         });
+      });
+    }
+
+    // UPDATE CARD
+    if (e.target.id.includes('update-card')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      console.warn('clicked edit button', e.target.id);
+      const payload = {
+        title: document.querySelector('#title-input').value,
+        language: document.querySelector('#language-input').value,
+        definition: document.querySelector('#definition-input').value,
+        firebaseKey,
+      };
+
+      updateCard(payload).then(() => {
+        getCardData(user.uid).then(showCards);
       });
     }
   });
