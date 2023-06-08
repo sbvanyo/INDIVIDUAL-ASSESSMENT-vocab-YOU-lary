5b5c0a276c2982fdf5565ac63aa3cd7fe1275fd1
Stacey Vanyo <sbvanyo@gmail.com>
2023-06-08 15:25:36 -0500

    language filters functional

--- components/shared/filterButtons.js
+++ components/shared/filterButtons.js
@@ -2,12 +2,12 @@ import renderToDOM from '../../utils/renderToDOM';
 
 const filterButtons = () => {
   const domString = `
-  <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-primary">Fortran</button>
-  <button type="button" class="btn btn-primary">JavaScript</button>
-  <button type="button" class="btn btn-primary">Python</button>
-  <button type="button" class="btn btn-primary">SQL</button>
-  <button type="button" class="btn btn-primary">All</button>
+<div id="filter-btn-group" class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-primary" id="fortran-btn">Fortran</button>
+  <button type="button" class="btn btn-primary" id="javascript-btn">JavaScript</button>
+  <button type="button" class="btn btn-primary" id="python-btn">Python</button>
+  <button type="button" class="btn btn-primary" id="sql-btn">SQL</button>
+  <button type="button" class="btn btn-primary" id="all-btn">All</button>
 </div>`;
 
   renderToDOM('#filter-buttons', domString);
